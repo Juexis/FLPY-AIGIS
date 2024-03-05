8a35e45e3c87e71f88e7c3747295c090cf5d23ee
Juexis <jasondo5806@yahoo.com>
2024-03-04 23:47:30 -0500

    Aigis flappable and barebones tower looping

---
+++ FLPY/Assets/Scripts/aigisLogic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aigisLogic : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float jumpForce; // positive y-value
+    public bool flapping;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) // if LMB is pressed
+        {
+            flapping = true;
+            body.velocity = new Vector2 (0, jumpForce);
+        }
+        else
+        {
+            flapping = false;
+        }
+    }
+}
